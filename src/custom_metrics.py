09a852559ea380936f513d78a4ac0462ef53efbd
hsm207 <hsm207@users.noreply.github.com>
2019-05-22 15:16:00 +0800

    Fix rmspe calculation bug

--- src/custom_metrics.py
+++ src/custom_metrics.py
@@ -1,9 +1,12 @@
-import custom_losses
 import tensorflow as tf
 
 
 def root_mean_squared_percentage_error(y_true, y_pred):
-    return tf.sqrt(custom_losses.mean_squared_percentage_error(y_true, y_pred))
+    # assume output from model is log scale and label is unscaled
+    y_pred = tf.exp(y_pred)
+    squared_prct = tf.square((y_pred - y_true)/y_true)
+
+    return tf.sqrt(tf.reduce_mean(squared_prct))
 
 
 rmspe = root_mean_squared_percentage_error
