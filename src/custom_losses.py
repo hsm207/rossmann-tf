81227894d7b9bba73cc3daba6a5e59efb8280584
hsm207 <hsm207@users.noreply.github.com>
2019-05-22 14:37:22 +0800

    Created mse on log loss function

--- src/custom_losses.py
+++ src/custom_losses.py
@@ -3,3 +3,9 @@ import tensorflow as tf
 
 def mean_squared_percentage_error(y_true, y_pred):
     return tf.reduce_sum(tf.square(tf.divide(y_true - y_pred, y_true)))
+
+def mse_log_log(y_true, y_pred):
+    y_true = tf.math.log(y_true + 1/1000)
+    y_pred = tf.math.log(y_pred + 1/1000)
+
+    return tf.reduce_sum(tf.square(y_pred - y_true))
