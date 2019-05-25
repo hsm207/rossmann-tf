d5728efab166dc784721f47da83c97b6defd6460
hsm207 <hsm207@users.noreply.github.com>
2019-05-25 20:57:12 +0800

    Created a learning rate finder callback

--- src/lr_schedules.py
+++ src/lr_schedules.py
@@ -1,10 +1,48 @@
 # Implement One Cycle Policy Algorithm in the Keras Callback Class
 # Adapted from https://www.kaggle.com/robotdreams/one-cycle-policy-with-keras
 
-import numpy as np
 import tensorflow as tf
 from tensorflow.keras import backend as K
 from tensorflow.keras.callbacks import *
+from numbers import Number
+from typing import Callable
+import numpy as np
+import sys
+import itertools
+import matplotlib.pyplot as plt
+
+AnnealFunc = Callable[[Number, Number, float], Number]
+
+
+def annealing_exp(start: float, end: float, pct: float) -> float:
+    return start * (end / start) ** pct
+
+
+def annealing_cos(start: float, end: float, pct: float) -> float:
+    cos_out = np.cos(np.pi * pct) + 1
+    return end + (start - end) / 2 * cos_out
+
+
+class Stepper:
+    def __init__(self, start: Number, end: Number, steps: int, anneal_func: AnnealFunc):
+        self.n = 0
+        self.total_steps = steps
+        self.start = start
+        self.end = end
+        self.anneal_func = anneal_func
+
+    def step(self) -> Number:
+        self.n = self.n + 1
+        progress = self.n / self.total_steps
+        next_val = self.anneal_func(start=self.start,
+                                    end=self.end,
+                                    pct=progress)
+
+        return next_val
+
+    @property
+    def is_done(self) -> bool:
+        return self.n >= self.total_steps
 
 
 class CyclicLR(tf.keras.callbacks.Callback):
@@ -81,3 +119,92 @@ class CyclicLR(tf.keras.callbacks.Callback):
 
         if self.cyclical_momentum == True:
             K.set_value(self.model.optimizer.momentum, self.cm())
+class LRFinder(tf.keras.callbacks.Callback):
+    def __init__(self, start_lr: float = 1e-7,
+                 end_lr: float = 10,
+                 num_iter: int = 100,
+                 stop_div: bool = True,
+                 anneal_func: AnnealFunc = annealing_cos):
+        super(LRFinder, self).__init__()
+        self.stop_div = stop_div
+        self.lr_schedule = Stepper(start=start_lr,
+                                   end=end_lr,
+                                   steps=num_iter,
+                                   anneal_func=anneal_func)
+        self.history = {}
+        self.loss_history = []
+        self.lr_history = []
+
+    def on_train_batch_begin(self, batch, logs=None):
+        lr = self.lr_schedule.step()
+        n = self.lr_schedule.n
+
+        self.lr_history.append(lr)
+
+        K.set_value(self.model.optimizer.lr, lr)
+
+    def on_train_batch_end(self, batch, logs=None):
+        loss = logs['loss']
+        self.loss_history.append(loss)
+
+        if self.lr_schedule.is_done:
+            self.model.stop_training = True
+
+    def on_epoch_end(self, batch, logs=None):
+        logs['loss_history'] = self.loss_history
+        logs['lr_history'] = self.lr_history
+
+        self.loss_history = []
+        self.lr_history = []
+
+
+def find_lr(model, ds, start_lr: float = 1e-7,
+            end_lr: float = 10,
+            num_iter: int = 100,
+            stop_div: bool = True,
+            anneal_func: AnnealFunc = annealing_exp):
+    """
+    Produce a plot to identify the maximum learning rate for one cycle training
+    :param model: A compiled Keras model
+    :param ds: A tf.dataset
+    :param start_lr: Minimum learning rate to begin search
+    :param end_lr: Maximum learning rate to end search at
+    :param num_iter: Number of steps to reach end_lr from start_lr
+    :param stop_div: Stop when loss starts to diverge (not implemented yet)
+    :param anneal_func: The annealing function to apply on the learning rate
+    :return: A matplotlib figure
+    """
+    lr_finder_callback = LRFinder(start_lr=start_lr,
+                                  end_lr=end_lr,
+                                  num_iter=num_iter,
+                                  anneal_func=anneal_func
+                                  )
+
+    # set to a really high epoch because we assume num_iter is a lot smaller
+    results = model.fit(ds,
+                        epochs=sys.maxsize,
+                        callbacks=[lr_finder_callback],
+                        verbose=1)
+
+    results.history.keys()
+    lr, loss = results.history['lr_history'], results.history['loss_history']
+    lr = list(itertools.chain(*lr))
+    loss = list(itertools.chain(*loss))
+    i = list(range(1, len(lr) + 1))
+
+    fig, axes = plt.subplots(2, 1)
+    fig.set_size_inches(h=8, w=10)
+
+    axes[0].plot(lr, loss)
+    axes[0].set_ylabel('Loss')
+    axes[0].set_xlabel('Learning Rate')
+    axes[0].set_xscale('log')
+    axes[0].xaxis.set_major_formatter(plt.FormatStrFormatter('%.0e'))
+
+    axes[1].plot(i, lr)
+    axes[1].set_ylabel('Learning Rate')
+    axes[1].set_xlabel('Step')
+
+    fig.savefig('./figures/viz.png')
+
+    return fig
